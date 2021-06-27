c71d42981326d0487bd04baca3375ce61b4c7883
Nguyễn Ngọc Đàm <dam.nguyen@icomm.vn>
2021-06-27 12:26:44 +0700

    fix generate yolo bounding boxes

--- synthesis_plate.py
+++ synthesis_plate.py
@@ -12,7 +12,9 @@ from aug import augmention
 
 available_number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 available_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-available_template = ['NN-CN/NNNN', 'NN-CN/NNN.NN', 'NNC/NNN.NN', 'NNC/NNNN', 'NNC-NNNN', 'NNC-NNN.NN']
+available_all = available_number + available_char
+# available_template = ['NN-CN/NNNN', 'NN-CN/NNN.NN', 'NNC/NNN.NN', 'NNC/NNNN', 'NNC-NNNN', 'NNC-NNN.NN']
+available_template = ['**-**/****', '**-**/***.**', '***/***.**', '***/****', '***-****', '***-***.**']
 available_square_bg = glob.glob('background/square*.jpg')
 available_rec_bg = glob.glob('background/rec*.jpg')
 
@@ -57,42 +59,51 @@ def sort_boxes(boxes, max_distance=0.3):
     return sorted_line_1 + sorted_line_2
 
 def segment_and_get_boxes(img, sample, textsize, margin = 3):
-	if type(textsize[0]).__name__ == 'tuple':
-		tmp = list(textsize).copy()
-		textsize = list(tmp[0])
-		textsize[0] = textsize[0] + tmp[1][0]
-	else:
-		textsize = list(textsize)
-	height, width, _ = img.shape
-	gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
-	_, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
-	_, contours, hier = cv2.findContours(thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	list_box = []
-	for i in range(len(contours)):
-		x, y, w, h = cv2.boundingRect(contours[i])
-		x += random.randint(-margin, 0)
-		y += random.randint(-margin, 0)
-		w += random.randint(0, 2*margin)
-		h += random.randint(0, 2*margin)
-
-		if h >= 0.95*textsize[1] and h <= 1.05*textsize[1]:
-			list_box.append([x, y, (x+w), (y+h)])
-	list_box = nms_fast(list_box)
-	list_box = format_boundingbox(list_box, width, height)
-	sorted_list_box = sort_boxes(list_box)
-	return sorted_list_box
+    total_char = len(sample.replace('.', '').replace('/', '').replace('-', ''))
+    if type(textsize[0]).__name__ == 'tuple':
+        tmp = list(textsize).copy()
+        textsize = list(tmp[0])
+        textsize[0] = textsize[0] + tmp[1][0]
+    else: 
+        textsize = list(textsize)
+    height, width, _ = img.shape
+    gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    thresh[:, 0:int(width*0.04)] = 0
+    thresh[:, int(width*0.96):] = 0
+    thresh[:int(height*0.05), :] = 0
+    thresh[int(height*0.95):, :] = 0
+    # cv2.imshow('thresh', thresh)
+    _, contours, hier = cv2.findContours(thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    sorted_contours = sorted(contours, key = cv2.contourArea, reverse = True)
+    list_box = []
+    for i in range(len(sorted_contours)):
+        x, y, w, h = cv2.boundingRect(sorted_contours[i])
+        x += random.randint(-margin, 0)
+        y += random.randint(-margin, 0)
+        w += random.randint(0, 2*margin)
+        h += random.randint(0, 2*margin)
+        list_box.append([x, y, (x+w), (y+h)])
+    list_box = nms_fast(np.array(list_box), overlapThresh = 0.1)[: total_char]
+    list_box = format_boundingbox(np.array(list_box), width, height)
+    sorted_list_box = sort_boxes(list_box)
+    return sorted_list_box
 
 
 def generate_sample(template):
-	count_numb = template.count('N')
-	count_char = template.count('C')
-	for i in range(count_numb):
-		idx_numb = random.randint(0, total_number - 1)
-		template = template.replace('N', available_number[idx_numb], 1)
-	for i in range(count_char):
-		idx_char = random.randint(0, total_char - 1)
-		template = template.replace('C', available_char[idx_char], 1)
-	return template
+    count_numb = template.count('N')
+    count_char = template.count('C')
+    count_all = template.count('*')
+    for i in range(count_numb):
+        idx_numb = random.randint(0, total_number - 1)
+        template = template.replace('N', available_number[idx_numb], 1)
+    for i in range(count_char):
+        idx_char = random.randint(0, total_char - 1)
+        template = template.replace('C', available_char[idx_char], 1)
+    for i in range(count_all):
+        idx_char = random.randint(0, total_char - 1)
+        template = template.replace('*', available_all[idx_char], 1)
+    return template
 
 def generate_plate(template):
 	if '/' in template:
@@ -131,7 +142,7 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='Vietnamese Synthesis License Plate.')
 
-	parser.add_argument('--numb', default=1000,
+	parser.add_argument('--numb', default=1,
 	                   help='Total number of Synthesis images')
 
 	parser.add_argument('--output_dir', default='output',
@@ -147,15 +158,15 @@ if __name__ == '__main__':
 			idx = random.randint(0, total_template - 1)
 			template = available_template[idx]
 			sample = generate_sample(template)
-			img, textsize = generate_plate(sample)
-			img = augmention(img)
-			width, height = img.size
-			boxes = segment_and_get_boxes(np.array(img), sample, textsize)
+			base_img, textsize = generate_plate(sample)
+			# aug_img = augmention(base_img)
+			width, height = base_img.size
+			boxes = segment_and_get_boxes(np.array(base_img), sample, textsize)
 			labels = sample.replace('-', '').replace('.', '').replace('/', '')
 			generate_yolo_label(boxes, labels, filename)
-			img.save(filename)
+			base_img.save(filename)
 			if visual:
-				visualize(np.array(img), boxes, labels)
+				visualize(np.array(base_img), boxes, labels)
 		except AssertionError:
 			err += 1
 	print('Completed !')
